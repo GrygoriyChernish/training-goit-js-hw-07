f7e497e46a64483cd23a0f6eb359a1ba9d324b86
Grigoriy Chernish <cga.jsdev@gmail.com>
2022-07-11 00:41:15 +0300

    bug fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.addEventListener('click', onGalleryImgClick);
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt='${description}'
+    />
+  </a>
+</div>`,
+    )
+    .join('');
+}
+
+function onEscButtonPress(evt) {
+  if (evt.key === 'Escape') {
+    instance.close();
+  }
+}
+
+function onGalleryImgClick(evt) {
+  evt.preventDefault();
+  const instance = basicLightbox.create(
+    `<img width="1400" height="900" src="${evt.target.dataset.source}">
+`,
+    {
+      onShow: () => {
+        document.addEventListener('keydown', onEscButtonPress);
+      },
+      onClose: () => {
+        document.removeEventListener('keydown', onEscButtonPress);
+      },
+    },
+  );
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+refs.gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  overlayOpacity: 1,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}"
+      data-source="${original}"
+      alt='${description}' />
+</a>`,
+    )
+    .join('');
+}
