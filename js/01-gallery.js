95531c0cf1bdc8ec1a1b5e2480fc157f5e6aaf72
Grigoriy Chernish <cga.jsdev@gmail.com>
2023-07-06 21:53:54 +0300

    add lazyLoading

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -47,6 +47,25 @@ function onGalleryImgClick(evt) {
   }
 }
 
+// if ('loading' in HTMLImageElement.prototype) {
+//   const images = document.querySelectorAll('img[loading="lazy"]');
+
+//   images.forEach(img => {
+//     img.src = img.dataset.src;
+//   });
+// } else {
+//   const script = document.createElement('script');
+
+//   script.src =
+//     'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
+//   script.integrity =
+//     'sha512-q583ppKrCRc7N5O0n2nzUiJ+suUv7Et1JGels4bXOaMFQcamPk9HjdUknZuuFjBNs7tsMuadge5k9RzdmO+1GQ==';
+//   script.crossorigin = 'anonymous';
+//   script.referrerpolicy = 'no-referrer';
+
+//   document.body.appendChild(script);
+// }
+
 // import { galleryItems } from './gallery-items.js';
 
 // const refs = {
